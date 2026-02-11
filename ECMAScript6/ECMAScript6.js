8091b9ed9c2123cc1339bd687611dc11832edca9
Mosaaed <mosaaedalsajer@gmail.com>
2026-02-11 15:15:05 +0100

    Add files via upload

---
+++ ECMAScript6/ECMAScript6.js
@@ -0,0 +1,85 @@
+// Exercise 1
+
+// let user={
+//     name: 'jahn',
+//     years:30
+
+// }
+// let {
+//     name,
+//     years:age,
+//     isAdmin = false
+// }=user;
+// console.log(name);
+// console.log(age);
+// console.log(isAdmin);
+
+
+// Exercise 2
+// const name = 'Earth';
+// let currtvist = 'jahn';
+// console.log(name);
+// console.log(currtvist);
+
+//Exercise 3
+// let phrase = "Hello"
+
+// if (true) {
+
+//     let user = "John";
+//      function sayHi() {
+//         alert(`${phrase}, ${user}`);
+//   }
+// }
+
+// sayHi();
+// it will give error 
+//because if into the say hi  block 
+
+
+
+
+// //exercise 4
+// let user ={};
+// user.name ='john';
+// user.surname = 'smith';
+// user.name= 'peter';
+// delete user.name;
+
+
+//ecercise 5
+
+// const user = {
+//   name: "John"
+// };
+
+// user.name = "Pete"; 
+// console.log(user.name); 
+
+
+//exercise 6
+
+// let slaries = {
+//     fred: 100,
+//     ted: 160,
+//     ghaith: 130
+// };
+// let sum = 0;
+// for (let totall in slaries){
+//     sum+=slaries[totall];
+// }
+// console.log(sum);
+
+
+//exercise7
+
+//if the condtion curect will be result below 
+//if not the condtion cuurect will be no thing 
+
+
+
+//Exercise 8
+
+//if the condtion first step will show hello
+//or if the next step condtion currect will be show greetings 
+//end if the list step conditon not currect default value '' no logic
