f641b322ea3f730d62db0dba54aee66179a4a231
Mosaaed <mosaaedalsajer@gmail.com>
2026-04-20 21:02:32 +0200

    Add files via upload

---
+++ exer1/ex.py
@@ -0,0 +1,26 @@
+#Create a variable age with your age and print it
+# my_old =30
+# print(my_old)
+
+
+#Store your name in a string variable and print "Hello, <your name>".
+# name="Mosaaed"
+# print("Hello "+name)
+
+
+
+#Make a list of 5 favorite fruits and print the second fruit.
+# fruits =["banana","mango","apple","orang","grapes"]
+# print(fruits[1])
+
+
+#Convert the string "123" into an integer and add 10 to it.
+
+# number=int("123")
+# rausult = number + 10
+# print(rausult)
+
+
+#Import the math module and print the square root of 16.
+import math
+print(math.sqrt(16))
