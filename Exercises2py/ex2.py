afb0cae55c5c92280c2286011ba0d63344fdb54d
Mosaaed <mosaaedalsajer@gmail.com>
2026-04-30 11:44:03 +0200

    Add files via upload

---
+++ Exercises2py/ex2.py
@@ -0,0 +1,30 @@
+# num = int(input("Enter a number: "))
+
+# if num > 0:
+#     print
+#     ("Positive")
+# elif num < 0:
+#     print("Negative")
+# else:
+#     print("Zero")
+
+
+
+# for i in range(1, 11):
+#     print(i)
+
+
+
+# i = 10
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+
+
+# num = int(input("Enter a number: "))
+
+# if num > 100:
+#     print("Big number!")
+# else:
+#     print("Small number!")
